af37dd7a9c0afc140443b6099ea1352858753b6e
usertest1 <user1@test.com>
2013-03-24 00:17:35 +0100

    commit 3 file

---
+++ exercice evaluation/Calculatrice.java
@@ -0,0 +1,16 @@
+package com.scja.exam.evaluation;
+public class Calculatrice{
+public static void main(String[] args){
+if()
+
+if(ags[0]=='-a')
+{
+
+}
+
+
+
+System.out.println("Greeting, Universe!");
+}
+
+}
---
+++ exercice1/GreetingsUniverse.java
@@ -0,0 +1,7 @@
+package com.scja.exam.tutorial;
+public class GreetingsUniverse{
+public static void main(String[] args){
+System.out.println("Greeting, Universe!");
+}
+
+}
